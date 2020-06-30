539e4ab34ee1c34c9d060897eda1e3b9cb915f2c
zkan <zkan.cs@gmail.com>
2020-06-30 22:14:47 +0700

    :sparkles: Introduce health check endpoint :bear:

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ODDS-TEAM/campfire-backend-api
 
 go 1.14
+
+require github.com/gorilla/mux v1.7.4 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	r := mux.NewRouter()
+	r.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "ok")
+	})
+
+	srv := &http.Server{
+		Handler: r,
+		Addr:    "0.0.0.0:8000",
+	}
+
+	srv.ListenAndServe()
 }
