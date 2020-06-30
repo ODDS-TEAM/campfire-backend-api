781120b29de9d1a04c79ae49b32e84f20ef7c627
zkan <zkan.cs@gmail.com>
2020-06-30 22:31:53 +0700

    :white_check_mark: Add testing health check handler :bear:

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/ODDS-TEAM/campfire-backend-api
 
 go 1.14
 
-require github.com/gorilla/mux v1.7.4 // indirect
+require github.com/gorilla/mux v1.7.4
--- main.go
+++ main.go
@@ -2,19 +2,25 @@ package main
 
 import (
 	"fmt"
-	"github.com/gorilla/mux"
 	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
 )
+
+func HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "ok")
+}
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "ok")
-	})
+	r.HandleFunc("/health", HealthCheckHandler)
 
 	srv := &http.Server{
-		Handler: r,
-		Addr:    "0.0.0.0:8000",
+		Handler:      r,
+		Addr:         "0.0.0.0:8000",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
 	}
 
 	srv.ListenAndServe()
